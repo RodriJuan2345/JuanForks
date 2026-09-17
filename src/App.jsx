bc448c2c9556e2859416c42162a2e699cc1619c6
RodriJuan2345 <millonlokilokimillon@gmail.com>
2026-09-17 16:48:36 -0400

    Juan:bugfux(footer):correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="pagina">
       <header>
-        <h1>Repositorios y Forks</h1>
+        <h1>Repositorios y Forks:Juan Rodrick</h1>
         <h1>Cuando hay limones</h1>
         <h1>el pepe quiere agua</h1>
       </header>
